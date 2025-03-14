e1c57919b8484e977185650b89f63580d8e7ae2d
joaonunovalente <jnvalente@ua.pt>
2025-03-14 21:43:34 +0000

    Remove google analytics

--- assets/vendor/startbootstrap-clean-blog/js/scripts.js
+++ assets/vendor/startbootstrap-clean-blog/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Clean Blog v5.1.0 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 (function ($) {
     "use strict"; // Start of use strict
 
