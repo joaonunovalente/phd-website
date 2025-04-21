33e2c43a064f0c1a5a3aa291d1b6249bbc3fa12a
joaonunovalente <jnvalente@ua.pt>
2025-04-21 01:41:17 +0100

    Add content to the PhD Research page

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,19 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+document.querySelectorAll('[data-toggle="collapse"]').forEach(trigger => {
+  trigger.addEventListener('click', function () {
+    const targetId = this.getAttribute('href') || this.getAttribute('data-target');
+    const header = this.closest('.card');
+
+    const content = document.querySelector(targetId);
+    if (!content.classList.contains('show')) {
+      setTimeout(() => {
+        const topPos = header.getBoundingClientRect().top + window.pageYOffset - 20; // Offset of 80px above
+        window.scrollTo({ top: topPos, behavior: 'smooth' });
+      }, 300);
+    }
+  });
+});
